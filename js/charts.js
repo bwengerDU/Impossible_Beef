8233f22c91edee304edc6692c6999a1d2fc0d278
Bryan Wenger <bryanwenger@Bryans-MacBook-Pro.local>
2022-03-17 15:05:49 -0600

    Deliverable 3 upload

--- js/charts.js
+++ js/charts.js
@@ -156,7 +156,7 @@ function buildCharts(sample) {
 
 
     // 3. Create a variable that holds the washing frequency.
-    var washingFreq = parsefloat(metaResult.wfreq);
+    var washingFreq = parseFloat(metaResult.wfreq);
     console.log(washingFreq);
    
     // Create the yticks for the bar chart.
@@ -168,7 +168,7 @@ function buildCharts(sample) {
     
     // Use Plotly to plot the bubble data and layout.
     Plotly.newPlot('bubble', bubbleData, bubbleLayout);
-   
+  
     
     // 4. Create the trace for the gauge chart.
     var gaugeData = [{
